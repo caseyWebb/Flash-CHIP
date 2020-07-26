5b7d5c173770be82e46883dffcf595f9bd784266
Casey Webb <notcaseywebb@gmail.com>
2020-07-26 15:23:15 -0500

    chore: remove os specific dep install

--- Flash.sh
+++ Flash.sh
@@ -36,45 +36,13 @@ echo " Then press enter please "
 
 read flavour
 
-
-
-echo -e "\n Setting up environment"
-sudo apt -y update
-sudo apt -y install \
- git \
- fastboot \
- u-boot-tools \
- adb \
- u-boot-tools \
- android-tools-fastboot \
- u-boot-tools \
- curl \
- wget \
- sunxi-tools \
-
-echo -e "\n Adding current user to dialout group"
-sudo usermod -a -G dialout $(logname)
-
-echo -e "\n Adding current user to plugdev group"
-sudo usermod -a -G plugdev $(logname)
-
-
-echo -e "\n Adding udev rule for Allwinner device"
-echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", GROUP="plugdev", MODE="0660" SYMLINK+="usb-serial-adapter"
-' | sudo tee /etc/udev/rules.d/99-allwinner.rules
-sudo udevadm control --reload-rules
-
 echo -e "\n Installing CHIP-tools"
 if [ -d CHIP-tools ]; then
  cd CHIP-tools 
  git pull 
- FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
- elif [ ! -d CHIP-tools ]; then
+elif [ ! -d CHIP-tools ]; then
  git clone https://github.com/Project-chip-crumbs/CHIP-tools.git
  cd  CHIP-tools 
- FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
 fi
 
+ FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
